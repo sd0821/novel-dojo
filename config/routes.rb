769f9ce6f07a3b01565fa70d28cca97cf1b091e8
sd0821 <68905745+sd0821@users.noreply.github.com>
2020-10-04 18:24:26 +0900

    マスターのコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "novels#index"
-  # root "titles#new"
+  # root "novels#index"
+  root "titles#new"
   # root "volumes#new"
   resources :users, only: [:edit, :update]
 end
